17e8169df2a1b9f743986065f7c8d170aecee67c
Hongsyn <hongsynk@gmail.com>
2017-09-27 23:31:56 -0400

    MVC layout created. Missing the public directory. Need to refine functionality.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ("New Bacon-ings Burger");
+INSERT INTO burgers (burger_name) VALUES ("Girls Just Wanna Have Fennel Burger");
+INSERT INTO burgers (burger_name) VALUES ("We're Here, We're Gruyere, Get Used To It Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blondes Have More Fun-gus Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bet It All On Black Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("If Looks Could Kale Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Itsy Bitsy Teeny Weeny Yellow Polka-Dot Zucchini Burger");
